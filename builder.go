44619ed8b4665a0ebbd33bfd2033c68fae24fdda
Benjamin Maynor <BenjaminMaynor@gmail.com>
2022-08-03 11:18:17 -0500

    Export Builder so it can be passed as a parameter

--- builder.go
+++ builder.go
@@ -22,15 +22,15 @@ var (
 	}
 )
 
-type builder struct {
+type Builder struct {
 	Dialect Dialect
 
 	Where []string
 	Args  []interface{}
 }
 
-func NewBuilder(d Dialect) *builder {
-	return &builder{Dialect: d}
+func NewBuilder(d Dialect) *Builder {
+	return &Builder{Dialect: d}
 }
 
 func boundParameter(d Dialect, n int) string {
@@ -38,7 +38,7 @@ func boundParameter(d Dialect, n int) string {
 }
 
 // BuildWhere just joins all the where strings. Join should be AND or OR
-func (b *builder) BuildWhere(join string) string {
+func (b *Builder) BuildWhere(join string) string {
 	if len(b.Where) < 1 {
 		return ""
 	}
@@ -55,7 +55,7 @@ func (b *builder) BuildWhere(join string) string {
 // WhereDistinct adds to the where clause a term that requires the value in the given field to be distinct from the given value.
 // Note that depending on your use case, you may need to cast your val to be what you expect.
 // e.g. casting a bool to an int.
-func (b *builder) WhereDistinct(field string, val interface{}) *builder {
+func (b *Builder) WhereDistinct(field string, val interface{}) *Builder {
 	b.Args = append(b.Args, val)
 	bp := boundParameter(b.Dialect, len(b.Args))
 
@@ -73,7 +73,7 @@ func (b *builder) WhereDistinct(field string, val interface{}) *builder {
 // WhereNotDistinct adds to the where clause a term that requires the value in the given field to be NOT distinct from the given value.
 // Note that depending on your use case, you may need to cast your val to be what you expect.
 // e.g. casting a bool to an int.
-func (b *builder) WhereNotDistinct(field string, val interface{}) *builder {
+func (b *Builder) WhereNotDistinct(field string, val interface{}) *Builder {
 	b.Args = append(b.Args, val)
 	bp := boundParameter(b.Dialect, len(b.Args))
 
@@ -89,40 +89,40 @@ func (b *builder) WhereNotDistinct(field string, val interface{}) *builder {
 }
 
 // WhereNull adds to the where clause a term that requires the value in the given field is null.
-func (b *builder) WhereNull(field string) *builder {
+func (b *Builder) WhereNull(field string) *Builder {
 	b.Where = append(b.Where, fmt.Sprintf(`%s IS NULL`, field))
 	return b
 }
 
 // WhereNotNull adds to the where clause a term that requires the value in the given field is not null.
-func (b *builder) WhereNotNull(field string) *builder {
+func (b *Builder) WhereNotNull(field string) *Builder {
 	b.Where = append(b.Where, fmt.Sprintf(`%s <> ''`, field))
 	return b
 }
 
 // WhereLess adds to the where clause a term that requires the value in the given field is less than the value.
-func (b *builder) WhereLess(field string, val interface{}) *builder {
+func (b *Builder) WhereLess(field string, val interface{}) *Builder {
 	b.Args = append(b.Args, val)
 	b.Where = append(b.Where, fmt.Sprintf(`%s < %s`, field, boundParameter(b.Dialect, len(b.Args))))
 	return b
 }
 
 // WhereLessEq adds to the where clause a term that requires the value in the given field is less than or equal to the value.
-func (b *builder) WhereLessEq(field string, val interface{}) *builder {
+func (b *Builder) WhereLessEq(field string, val interface{}) *Builder {
 	b.Args = append(b.Args, val)
 	b.Where = append(b.Where, fmt.Sprintf(`%s <= %s`, field, boundParameter(b.Dialect, len(b.Args))))
 	return b
 }
 
 // WhereGreater adds to the where clause a term that requires the value in the given field is greater than the value.
-func (b *builder) WhereGreater(field string, val interface{}) *builder {
+func (b *Builder) WhereGreater(field string, val interface{}) *Builder {
 	b.Args = append(b.Args, val)
 	b.Where = append(b.Where, fmt.Sprintf(`%s > %s`, field, boundParameter(b.Dialect, len(b.Args))))
 	return b
 }
 
 // WhereGreaterEq adds to the where clause a term that requires the value in the given field is greater than or equal to the value.
-func (b *builder) WhereGreaterEq(field string, val interface{}) *builder {
+func (b *Builder) WhereGreaterEq(field string, val interface{}) *Builder {
 	b.Args = append(b.Args, val)
 	b.Where = append(b.Where, fmt.Sprintf(`%s >= %s`, field, boundParameter(b.Dialect, len(b.Args))))
 	return b
@@ -130,7 +130,7 @@ func (b *builder) WhereGreaterEq(field string, val interface{}) *builder {
 
 // WhereExact adds to the where clause a term that requires the value in the given field to exactly match the given value.
 // If you want to LOWER your field for matching, wrap the field name in LOWER() when passing it in (mssql, psql)
-func (b *builder) WhereExact(field string, val interface{}) *builder {
+func (b *Builder) WhereExact(field string, val interface{}) *Builder {
 	b.Args = append(b.Args, val)
 	b.Where = append(b.Where, fmt.Sprintf(`%s = %s`, field, boundParameter(b.Dialect, len(b.Args))))
 	return b
@@ -138,7 +138,7 @@ func (b *builder) WhereExact(field string, val interface{}) *builder {
 
 // WhereExact adds to the where clause a term that requires the value in the given field to NOT exactly match the given.
 // If you want to LOWER your field for matching, wrap the field name in LOWER() when passing it in (mssql, psql).
-func (b *builder) WhereNotExact(field string, val interface{}) *builder {
+func (b *Builder) WhereNotExact(field string, val interface{}) *Builder {
 	b.Args = append(b.Args, val)
 	b.Where = append(b.Where, fmt.Sprintf(`%s != %s`, field, boundParameter(b.Dialect, len(b.Args))))
 	return b
@@ -146,7 +146,7 @@ func (b *builder) WhereNotExact(field string, val interface{}) *builder {
 
 // WhereLike adds to the where clause a term that requires the value in the given field to at least partially match the values given.
 // If you want to LOWER your field for matching, wrap the field name in LOWER() when passing it in (mssql, psql).
-func (b *builder) WhereLike(field string, vals ...string) *builder {
+func (b *Builder) WhereLike(field string, vals ...string) *Builder {
 	where := make([]string, len(vals))
 	for _, val := range vals {
 		val = "%" + val + "%"
@@ -160,7 +160,7 @@ func (b *builder) WhereLike(field string, vals ...string) *builder {
 
 // WhereNotLike adds to the where clause a term that requires that the value in the field does NOT partially match the values given.
 // If you want to LOWER your field for matching, wrap the field name in LOWER() when passing it in (mssql, psql).
-func (b *builder) WhereNotLike(field string, vals ...string) *builder {
+func (b *Builder) WhereNotLike(field string, vals ...string) *Builder {
 	where := make([]string, len(vals))
 	for _, val := range vals {
 		val = "%" + val + "%"
@@ -174,7 +174,7 @@ func (b *builder) WhereNotLike(field string, vals ...string) *builder {
 
 // WhereIn adds to the where clause a term that requires that the values in the given field exactly match one or more of the values given.
 // If you want to LOWER your field for matching, wrap the field name in LOWER() when passing it in (mssql, psql).
-func (b *builder) WhereIn(field string, vals ...interface{}) *builder {
+func (b *Builder) WhereIn(field string, vals ...interface{}) *Builder {
 	if len(vals) == 0 {
 		return b
 	}
@@ -192,7 +192,7 @@ func (b *builder) WhereIn(field string, vals ...interface{}) *builder {
 
 // WhereNotIn adds to the where clause a term that requires that the values in the given field exactly match none of the values given.
 // If you want to LOWER your field for matching, wrap the field name in LOWER() when passing it in (mssql, psql).
-func (b *builder) WhereNotIn(field string, vals ...interface{}) *builder {
+func (b *Builder) WhereNotIn(field string, vals ...interface{}) *Builder {
 	if len(vals) == 0 {
 		return b
 	}
@@ -210,7 +210,7 @@ func (b *builder) WhereNotIn(field string, vals ...interface{}) *builder {
 
 // AddToArgs simply adds additional parameters to the Args array. This will have an impact on the bound parameter count of any subsequent WHERE clauses added.
 // This is useful for things like subqueries where you're not directly matching against a single field.
-func (b *builder) AddToArgs(vals ...interface{}) *builder {
+func (b *Builder) AddToArgs(vals ...interface{}) *Builder {
 	var ps []string
 	for _, v := range vals {
 		b.Args = append(b.Args, v)
